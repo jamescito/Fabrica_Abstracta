87dae7bc48e50b85876ded038b2546041d2866f6
jamescito <jr76407900@gmail.com>
2021-10-12 13:58:45 -0600

    Agregue la clase de Motor y Carroceria

--- src/main/java/org/james/FordFocusFactory.java
+++ src/main/java/org/james/FordFocusFactory.java
@@ -1,6 +1,18 @@
 package org.james;
 
-public class FordFocusFactory{
+public class FordFocusFactory implements CocheFactory{
+
+    @Override
+    public Motor crearMotor() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Carroceria crearCarroceria() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
 
 
